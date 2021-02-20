37a4be46007ee4c30070309baf45e14f3b242df3
Dzmitry Shakhno <Dzmitry_Shakhno@epam.com>
2021-02-20 11:22:54 +0300

    env setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import env from "./env/config.env.json"
 export default class App extends React.Component {
   render() {
     return React.createElement(
       "div",
       null,
-      `Hello World Task 1 React.CreateElement`
+      env.message
     );
   }
 }
---
+++ webpack.prod.js
@@ -0,0 +1,17 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const webpack = require("webpack");
+module.exports = merge(common, {
+  mode: "production",
+  plugins: [
+    new webpack.NormalModuleReplacementPlugin(
+      /(.*)config\.env(\.*)/,
+      function (resource) {
+        resource.request = resource.request.replace(
+          "config.env",
+          "config.env.prod"
+        );
+      }
+    ),
+  ],
+});
